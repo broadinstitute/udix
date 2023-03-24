b450d7699dd9223c788fba7efd2b95af4543aed5
curoli <curoli@gmail.com>
2023-03-24 08:51:56 -0400

    Adding job state "restartable".

--- src/job.rs
+++ src/job.rs
@@ -5,6 +5,7 @@ use crate::error::Error;
 #[derive(Deserialize, PartialEq)]
 #[serde(try_from = "&str")]
 pub(crate) enum JobState {
+    Restartable,
     Runnable,
     Running,
     Failed,
@@ -12,6 +13,7 @@ pub(crate) enum JobState {
 }
 
 mod names {
+    pub(crate) const RESTARTABLE: &str = "restartable";
     pub(crate) const RUNNABLE: &str = "runnable";
     pub(crate) const RUNNING: &str = "running";
     pub(crate) const FAILED: &str = "failed";
@@ -24,6 +26,7 @@ impl TryFrom<&str> for JobState {
 
     fn try_from(string: &str) -> Result<Self, Self::Error> {
         match string {
+            names::RESTARTABLE => { Ok(JobState::Restartable) }
             names::RUNNABLE => { Ok(JobState::Runnable) }
             names::RUNNING => { Ok(JobState::Running) }
             names::FAILED => { Ok(JobState::Failed) }
@@ -42,6 +45,7 @@ impl TryFrom<&str> for JobState {
 impl Display for JobState {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            JobState::Restartable => { write!(f, "{}", names::RESTARTABLE) }
             JobState::Runnable => { write!(f, "{}", names::RUNNABLE) }
             JobState::Running => { write!(f, "{}", names::RUNNING) }
             JobState::Failed => { write!(f, "{}", names::FAILED) }
