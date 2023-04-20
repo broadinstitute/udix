0a9b1f6a5ba29c4b2aee6c0ea0c9e2a062c85306
curoli <curoli@gmail.com>
2023-04-20 17:09:17 -0400

    bedmerge, part 5.

--- src/data/beds.rs
+++ src/data/beds.rs
@@ -4,11 +4,13 @@ use crate::dx;
 use crate::error::Error;
 
 enum FileType {
-    Bed, Bim, Fam
+    Bed,
+    Bim,
+    Fam,
 }
 
 pub(crate) struct BedBundle {
-    basename: String
+    basename: String,
 }
 
 impl Display for BedBundle {
@@ -32,13 +34,20 @@ impl FileMatchBuffer {
         let got_fam = false;
         FileMatchBuffer { basename, got_bed, got_bim, got_fam }
     }
+    fn got_file_type(&mut self, file_type: &FileType) -> &mut bool {
+        match file_type {
+            FileType::Bed => { &mut self.got_bed }
+            FileType::Bim => { &mut self.got_bim }
+            FileType::Fam => { &mut self.got_fam }
+        }
+    }
     fn push(&mut self, file_type: &FileType, basename: &str) -> Result<Option<BedBundle>, Error> {
         todo!()
     }
     fn unmatched_error(&self) -> Error {
         todo!()
     }
-    fn is_empty(&self) -> bool { (!self.got_bed) &&(!self.got_bim) &&(!self.got_fam) }
+    fn is_empty(&self) -> bool { (!self.got_bed) && (!self.got_bim) && (!self.got_fam) }
 }
 
 fn get_bed_bundles(conf: &Conf) -> Result<Vec<BedBundle>, Error> {
@@ -46,21 +55,28 @@ fn get_bed_bundles(conf: &Conf) -> Result<Vec<BedBundle>, Error> {
     let mut lines: Vec<&str> = stdout.lines().collect();
     lines.sort();
     let mut bed_bundles: Vec<BedBundle> = Vec::new();
-    let mut basename_opt: Option<&str> = None;
-    let mut got_bed = false;
-    let mut got_bim = false;
-    let mut got_fam = false;
+    let mut file_match_buffer = FileMatchBuffer::new();
     for line in lines {
-        if let Some(bed_basename) = line.strip_suffix(".bed") {
-
-        } else if let Some(bim_basename) = line.strip_suffix(".bim") {
-            bim_basenames.push(bim_basename)
-        } else if let Some(fam_basename) = line.strip_suffix(".fam") {
-            fam_basenames.push(fam_basename)
+        let bed_bundle_opt_res =
+            if let Some(bed_basename) = line.strip_suffix(".bed") {
+                file_match_buffer.push(&FileType::Bed, bed_basename)
+            } else if let Some(bim_basename) = line.strip_suffix(".bim") {
+                file_match_buffer.push(&FileType::Bim, bim_basename)
+            } else if let Some(fam_basename) = line.strip_suffix(".fam") {
+                file_match_buffer.push(&FileType::Fam, fam_basename)
+            } else {
+                Ok(None)
+            };
+        if let Some(file_bundle) = bed_bundle_opt_res? {
+            bed_bundles.push(file_bundle)
         }
         println!("{}", line);
     }
-    Ok(bed_bundles)
+    if file_match_buffer.is_empty() {
+        Ok(bed_bundles)
+    } else {
+        Err(file_match_buffer.unmatched_error())
+    }
 }
 
 pub(crate) fn list_beds(conf: &Conf) -> Result<(), Error> {
